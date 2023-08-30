eb356e14e8d1d2a2d9ff264525a1e2fa2c7d8e77
YkRic <jessika.palomares@gmail.com>
2023-08-29 21:59:46 -0300

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner; 
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    // 1 a
+	    if(6<6*5)
+	        System.out.println("Hello");
+	        System.out.println(" There");
+	        
+	    // Respuesta: Hello
+	    // there
+	    
+	    /* 1 b*/
+	    
+	    int x=1, y=2, z=3; 
+	    
+	    if(x>y)
+	    if(x>z)
+	        System.out.println("11111");
+	    else
+	        System.out.println("22222");
+	   // Respuesta : no imprime nada
+	   
+	   /**1 c*/
+	    
+	}
+}
